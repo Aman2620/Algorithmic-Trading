c6bb8498bfd50185721713d1ea6126ceaac91ea0
Aman <shrivastavaman005@gmail.com>
2024-01-12 14:28:33 +0530

    Initial Commit

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from home import views
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('index', views.index,name='index'),
+]
